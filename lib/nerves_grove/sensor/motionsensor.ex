8703b5584f682b7d81304b09a309ce1b031851a5
Manuel <>
2018-09-02 14:51:53 +0200

    Added motion sensor control

--- lib/nerves_grove/sensor/motionsensor.ex
+++ lib/nerves_grove/sensor/motionsensor.ex
@@ -1,32 +1,15 @@
-defmodule Grove_sensor_alarm do
-
+defmodule Nerves.Grove.Sensor.MotionSensor do
   alias ElixirALE.GPIO
 
-  def setup() do
-  GPIO.setmode(GPIO.BCM)
-
-  GPIO.setup(23, GPIO.IN) #PIR
-  GPIO.setup(24, GPIO.OUT) #Buzzer
-end
-
-
-  def start_alarm do
-      try do
-        {time, res} = :timer.tc fn -> :timer.sleep(1) end
-        if GPIO.input(23) do
-          GPIO.output(24, true)
-          {time, res} = :timer.tc fn -> :timer.sleep(0.5) end #Buzzer turns on for 0.5 secs
-          GPIO.output(24, false)
-          IO.puts("Motion detected...")
-          {time, res} = :timer.tc fn -> :timer.sleep(5) end#To avoid multipe detection
-        end
-      {time, res} = :timer.tc fn -> :timer.sleep(0.1) end#Loop delay
-      catch
-      :exit, _ -> "not really"
-      end
-
-
-
+  def start_link(pin) when is_integer(pin) do
+    GPIO.start_link(pin, :output)
   end
 
+  def on(pid) do
+    GPIO.read(pid)
+  end
+
+  def off(pid) do
+    GPIO.read(pid)
+  end
 end
