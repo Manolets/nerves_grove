a24977b7315217f825c5f49f7ef81c62e608419f
taorg <antonio.lopez@ptbsl.com>
2018-09-03 14:40:18 +0200

    MotionSensor fixed

--- lib/nerves_grove/sensor/motionsensor.ex
+++ lib/nerves_grove/sensor/motionsensor.ex
@@ -1,13 +1,23 @@
 defmodule Nerves.Grove.Sensor.MotionSensor do
   alias ElixirALE.GPIO
 
+  @moduledoc """
+  Seeed Studio [Grove MotionSensor Sensor]
+  # Example
+
+      alias Nerves.Grove.Sensor
+
+      {:ok, pid} = Sensor.MotionSensor.start_link(pin)
+
+      state = Sensor.MotionSensor.read(pid)  # check if sensor was triggered
+  """
   @spec start_link(pos_integer) :: {:ok, pid} | {:error, any}
   def start_link(pin) when is_integer(pin) do
-    GPIO.start_link(pin, :output)
+    GPIO.start_link(pin, :input)
   end
 
-  def read(pid) do
-    GPIO.read(pid)
+  @spec read(pid) :: boolean
+  def read(pid) when is_pid(pid) do
+    GPIO.read(pid) == 0
   end
-
 end
