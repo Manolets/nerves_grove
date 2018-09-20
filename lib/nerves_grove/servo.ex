93ff3e5f0029cf3dd66386463ce8e014f687813a
taorg <antonio.lopez@ptbsl.com>
2018-09-20 22:48:41 +0200

    Not perfect, but first working version

--- lib/nerves_grove/servo.ex
+++ lib/nerves_grove/servo.ex
@@ -26,7 +26,7 @@ defmodule Nerves.Grove.Servo do
   """
 
   require Logger
-  alias Pigpiox
+  alias Pigpiox.Waveform
 
   def rotate(pin, angle) do
     Task.async(fn -> move(pin, angle) end)
@@ -38,18 +38,19 @@ defmodule Nerves.Grove.Servo do
     delay2 = 20000 - delay1
 
     pulses = [
-      %Pigpiox.Waveform.Pulse{gpio_on: pin, delay: delay1},
-      %Pigpiox.Waveform.Pulse{gpio_off: pin, delay: delay2}
+      %Waveform.Pulse{gpio_on: pin, delay: delay1},
+      %Waveform.Pulse{gpio_off: pin, delay: delay2}
     ]
 
-    with {:ok, wf_id} <- Pigpiox.Waveform.add_generic(pulses),
+    with {:ok, wfa_id} <- Waveform.add_generic(pulses),
          {:ok, wave_id} <- Pigpiox.Waveform.create(),
          :ok <- Pigpiox.GPIO.set_mode(pin, :output),
-         do: Pigpiox.Waveform.repeat(wave_id)
+         {:ok, wf_id} <- Waveform.repeat(wave_id),
+         {:ok, micros} <- Waveform.get_micros(),
+         do: Process.sleep(150)
 
     Logger.debug("Pin introduced = #{inspect(pin)} ")
     Logger.debug("Pulses to output = #{inspect(pulses)} ")
-    Process.sleep(20)
   end
 
   def stop() do
