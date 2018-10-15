3ba5586b5487a03269a8cf2f30f2eae716d67047
Manuel <>
2018-10-15 22:06:21 +0200

    Changed some atoms

--- lib/nerves_grove/pca9685/tetrapod.ex
+++ lib/nerves_grove/pca9685/tetrapod.ex
@@ -3,24 +3,24 @@ defmodule Nerves.Grove.PCA9685.Tetrapod do
   require Logger
   @devices [%{bus: 1, address: 0x40, pwm_freq: 50}]
   @servos [
-    %{limb: :frbody, bus: 1, address: 0x40, channel: 0, position: 90, min: 175, max: 575},
-    %{limb: :frhip, bus: 1, address: 0x40, channel: 1, position: 90, min: 175, max: 575},
-    %{limb: :frknee, bus: 1, address: 0x40, channel: 2, position: 90, min: 175, max: 575},
-    %{limb: :flbody, bus: 1, address: 0x40, channel: 3, position: 90, min: 175, max: 575},
-    %{limb: :flhip, bus: 1, address: 0x40, channel: 4, position: 90, min: 175, max: 575},
-    %{limb: :flknee, bus: 1, address: 0x40, channel: 5, position: 90, min: 175, max: 575},
-    %{limb: :blbody, bus: 1, address: 0x40, channel: 6, position: 90, min: 175, max: 575},
-    %{limb: :blhip, bus: 1, address: 0x40, channel: 7, position: 90, min: 175, max: 575},
-    %{limb: :blknee, bus: 1, address: 0x40, channel: 8, position: 90, min: 175, max: 575},
-    %{limb: :brbody, bus: 1, address: 0x40, channel: 9, position: 90, min: 175, max: 575},
-    %{limb: :rhip, bus: 1, address: 0x40, channel: 10, position: 90, min: 175, max: 575},
-    %{limb: :brknee, bus: 1, address: 0x40, channel: 11, position: 90, min: 175, max: 575}
+    %{limb: :frb, bus: 1, address: 0x40, channel: 0, position: 90, min: 175, max: 575},
+    %{limb: :frh, bus: 1, address: 0x40, channel: 1, position: 90, min: 175, max: 575},
+    %{limb: :frk, bus: 1, address: 0x40, channel: 2, position: 90, min: 175, max: 575},
+    %{limb: :brb, bus: 1, address: 0x40, channel: 3, position: 90, min: 175, max: 575},
+    %{limb: :brh, bus: 1, address: 0x40, channel: 4, position: 90, min: 175, max: 575},
+    %{limb: :brk, bus: 1, address: 0x40, channel: 5, position: 90, min: 175, max: 575},
+    %{limb: :blb, bus: 1, address: 0x40, channel: 6, position: 90, min: 175, max: 575},
+    %{limb: :blh, bus: 1, address: 0x40, channel: 7, position: 90, min: 175, max: 575},
+    %{limb: :blk, bus: 1, address: 0x40, channel: 8, position: 90, min: 175, max: 575},
+    %{limb: :flb, bus: 1, address: 0x40, channel: 9, position: 90, min: 175, max: 575},
+    %{limb: :flh, bus: 1, address: 0x40, channel: 10, position: 90, min: 175, max: 575},
+    %{limb: :flk, bus: 1, address: 0x40, channel: 11, position: 90, min: 175, max: 575}
   ]
   @moduledoc """
   Tetrapod utils
   RingLogger.attach()
   Nerves.Grove.PCA9685.Tetrapod.start_shield()
-  Nerves.Grove.PCA9685.Servo (
+  Nerves.Grove.PCA9685.Servo.position(
     Nerves.Grove.PCA9685.Tetrapod.limb_id(:brknee),90
   )
   """
